203e5334f148e704c4afedc15496b3b31abefeb3
parth1985 <42909356+parth1985@users.noreply.github.com>
2018-09-02 17:25:45 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,13 +1,42 @@
 <?php
+
+
 session_start();
-if(isset($_request("uname"))&&(isset(_
-
-
-
-
-
-
-
-
-
+if(isset($_REQUEST["uname"])&&isset($_REQUEST["upass"]))
+	
+{
+	$user=$_REQUEST["uname"];
+	$pass=$_REQUEST["upass"];
+}
+if($user=="raj"&&$pass=="ss")
+{
+$_SESSION["u"]=$user;
+	header("location:inbox1.php");
+}
+else if($user=="admin"&& $pass=="admin")
+{$_SESSION["u"]=$user;
+header("location:inbox1.php");	
+}
+else
+{echo "invalid user";	
+}
 ?>
+
+<html>
+<head>
+<link rel="stylesheet" href="reg.css">
+</head>
+<center>
+<body background=red>
+
+<form method="GET" action="login1.php">
+username::<b><input type="text" name="uname"/></b><br><br>
+password::<input type="text" name="upass"/><br><br>
+<input type="submit" name="usubmit" value="login"/><br><br>
+<a href="registration.php">signup</a><br><br>
+<a href="forgot.php">forgot password</a><br><br>
+</form>
+
+</body>
+</center>
+</html>
